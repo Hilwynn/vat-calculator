8d6863a56a39ad242f447237c9518db1095d70a8
Anna Erkers <31205429+Hilwynn@users.noreply.github.com>
2018-10-16 18:52:45 +0200

    Updates

---
+++ precache-manifest.ff0eae813f2b74bc936cccae72ea0487.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "efe390d8637b697acc75",
+    "url": "/vat-calculator/static/js/runtime~main.efe390d8.js"
+  },
+  {
+    "revision": "a31ce716cd5becaeed72",
+    "url": "/vat-calculator/static/js/main.a31ce716.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/vat-calculator/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "a31ce716cd5becaeed72",
+    "url": "/vat-calculator/static/css/main.30721a03.chunk.css"
+  },
+  {
+    "revision": "313cf71bb5e073b704d96f8a666d16d0",
+    "url": "/vat-calculator/index.html"
+  }
+];
